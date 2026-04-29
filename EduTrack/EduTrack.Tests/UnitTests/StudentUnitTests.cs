9379419e449bb427cdf04d27cd2ce2d5119957b7
d-luan <dluan1285@gmail.com>
2026-04-29 00:02:53 -0300

    refactor(test): change the test structure and simplifies the documentation

---
+++ EduTrack/EduTrack.Tests/UnitTests/StudentUnitTests.cs
@@ -0,0 +1,18 @@
+﻿using EduTrack.Api.Models;
+
+namespace EduTrack.Tests.UnitTests;
+
+public class StudentUnitTests
+{
+    [Fact]
+    public void EnrollIn_SameCourseTwice_ShouldThrowInvalidOperationException()
+    {
+        // Arrange
+        Student robert = new("Robert Smith", "robert234@email.com");
+        Course computerScience = new("Computer Science");
+        robert.EnrollIn(computerScience);
+
+        // Act & Assert
+        Assert.Throws<InvalidOperationException>(() => robert.EnrollIn(computerScience));
+    }
+}
