e559d4b7ea6f0d967c82dacee0a15824d32bd5d0
d-luan <dluan1285@gmail.com>
2026-05-04 23:39:48 -0300

    feat(repository): add create and getall methods to the course repository

---
+++ src/EduTrack.Api/Repositories/CourseRepository.cs
@@ -0,0 +1,37 @@
+﻿using EduTrack.Api.Data;
+using EduTrack.Api.Models;
+using Dapper;
+
+namespace EduTrack.Api.Repositories;
+
+public class CourseRepository(ISqlConnectionFactory connectionFactory) : ICourseRepository
+{
+    public async Task<int> CreateAsync(Course course)
+    {
+        using var connection = connectionFactory.CreateConnection();
+
+        var sql = @"
+            INSERT INTO Course (Name, Description)
+            OUTPUT INSERTED.Id
+            VALUES (@Name, @Description);
+        ";
+
+        var generatedId = await connection.ExecuteScalarAsync<int>(
+            sql,
+            param: new
+            {
+                Name = course.Name,
+                Description = course.Description
+            }
+        );
+
+        return generatedId;
+    }
+
+    public async Task<IEnumerable<Course>> GetAllAsync()
+    {
+        using var connection = connectionFactory.CreateConnection();
+        var sql = "SELECT Id, Name, Description FROM Course;";
+        return await connection.QueryAsync<Course>(sql);
+    }
+}
---
+++ src/EduTrack.Api/Repositories/ICourseRepository.cs
@@ -0,0 +1,9 @@
+﻿using EduTrack.Api.Models;
+
+namespace EduTrack.Api.Repositories;
+
+public interface ICourseRepository
+{
+    Task<int> CreateAsync(Course course);
+    Task<IEnumerable<Course>> GetAllAsync();
+}
