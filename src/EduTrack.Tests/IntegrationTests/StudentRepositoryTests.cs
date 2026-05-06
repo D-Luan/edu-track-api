37e1686cbd24ecc0b775cdf8a12bbf2fb6c38ca8
d-luan <dluan1285@gmail.com>
2026-05-05 21:48:00 -0300

    refactor(test): add TestConnectionFactory class for integration test

--- src/EduTrack.Tests/IntegrationTests/StudentRepositoryTests.cs
+++ src/EduTrack.Tests/IntegrationTests/StudentRepositoryTests.cs
@@ -1,17 +1,10 @@
-﻿using System.Data;
-using Microsoft.Data.SqlClient;
+﻿using Microsoft.Data.SqlClient;
 using EduTrack.Api.Models;
 using EduTrack.Api.Repositories;
-using EduTrack.Api.Data;
 using EduTrack.Tests.Fixtures;
 using Dapper;
 
 namespace EduTrack.Tests.IntegrationTests;
-
-public class TestConnectionFactory(string connectionString) : ISqlConnectionFactory
-{
-    public IDbConnection CreateConnection() => new SqlConnection(connectionString);
-}
 
 public class StudentRepositoryTests(DatabaseFixture fixture) : IClassFixture<DatabaseFixture>
 {
---
+++ src/EduTrack.Tests/IntegrationTests/TestConnectionFactory.cs
@@ -0,0 +1,10 @@
+﻿using EduTrack.Api.Data;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace EduTrack.Tests.IntegrationTests;
+
+public class TestConnectionFactory(string connectionString) : ISqlConnectionFactory
+{
+    public IDbConnection CreateConnection() => new SqlConnection(connectionString);
+}
